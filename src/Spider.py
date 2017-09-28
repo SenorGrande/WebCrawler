c52fefa9fb255f913cd7cfddc13a18e49d5092b9
SenorGrande <cvf3200@autuni.ac.nz>
2017-09-28 06:24:36 +0000

    Starting on Spider

--- src/Spider.py
+++ src/Spider.py
@@ -3,3 +3,21 @@ Created on 16/09/2017
 
 @author: Connor
 '''
+
+import SpiderLeg
+
+depth = 5
+unvisited = ['http://www.android.com/'] # needs to handle multiple
+visited = []
+
+spider = SpiderLeg.SpiderLeg()
+
+# loops through URLs in unvisited, adding them to visited
+for i in range(len(unvisited)):
+    url = unvisited.pop(0)
+    visited.append(url)
+
+    test = spider.getTitle(url)
+    test = spider.getHyperlink(url)
+    test = spider.getImages(url)
+    test = spider.getMeta(url)
--- src/SpiderLeg.py
+++ src/SpiderLeg.py
@@ -7,8 +7,6 @@ Created on 16/09/2017
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-print("testing")
-
 class SpiderLeg:
     
     def __init__(self):
@@ -25,26 +23,16 @@ class SpiderLeg:
         for link in soup.find_all('a'):
             print(link.get('href'))
         
-        
-    def getImages(self, url):
-        print("get names of all image files in the web page url, as well as the height, width and alt attributes")
+    def getImages(self, url):    
         page = urlopen(url)
         soup = BeautifulSoup(page, 'lxml')
         for link in soup.find_all('img'):
-            print(link.get('src'))
-        
+            print(link.get('src'))    
         
     def getMeta(self, url):
-        print("get the meta data, including meta description, and meta keywords of the web page url")
         page = urlopen(url)
         soup = BeautifulSoup(page, 'lxml')
         for link in soup.find_all('meta'):
             print(link.get('name'))
 
-
-url = 'http://www.ace-hire.com/'
-test = SpiderLeg().getTitle(url)
-test = SpiderLeg().getHyperlink(url)
-test = SpiderLeg().getImages(url)
-test = SpiderLeg().getMeta(url)
 

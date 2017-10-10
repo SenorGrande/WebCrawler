f0a3bccbb2071f6906375863ce6223537087d631
SenorGrande <cvf3200@autuni.ac.nz>
2017-10-10 03:10:30 +0000

    Created SearchTool which is a basic command line search tool and text file links.txt to store results in

--- src/Spider.py
+++ src/Spider.py
@@ -10,66 +10,63 @@ Created on 16/09/2017
 import SpiderLeg
 from urllib.parse import urlparse
 
-# need to include depth, first one will be 0
-unvisited = [[0,'http://sarsaparilla/crawlTest.html']] # needs to handle multiple
-visited = [] # stores hyperlinks that have been visited
-links = [] # stores the hyperlinks found on a page
-maxDepth = 5 # the max depth the crawl can go
-depth = 0 # the current depth of the crawl
-keyword = '' # keyword we are searching
-
-images = []
-meta = []
-
-spiderleg = None
-
 class Spider:
 
 	def __init__(self):
 		print("=== Spider ===")
+		depth = 0 # the current depth of the crawl
+		unvisited = None
+		visited = []
+		maxDepth = None
+		keyword = None
+		spiderleg = SpiderLeg.SpiderLeg()
+		images = []
+		meta = []
 
 	def setup(self, ulinks, udepth, ukeyword):
 		print("=== Setup ===")
-		unvisited = ulinks
-		maxDepth = udepth
-		keyword = ukeyword
+		self.unvisited = ulinks
+		self.maxDepth = udepth
+		self.keyword = ukeyword
 		
 	def crawl(self):
 		print("=== Crawl ===")
 		
-		spiderleg = SpiderLeg.SpiderLeg() # MOVE???
-		
 		# loops through URLs in unvisited, adding them to visited
-		while len(unvisited) != 0:
+		while len(self.unvisited) != 0:
 			print("====================")
-			url = unvisited.pop(0)
-			depth = url[0] # this will get depth value of current URL
-			depth+=1 # depth for the links we will be adding
-			visited.append(url[1]) # does visited need the depth as well?
+			url = self.unvisited.pop(0)
+			self.depth = url[0] # this will get depth value of current URL
+			self.depth+=1 # depth for the links we will be adding
+			self.visited.append(url[1]) # does visited need the depth as well?
 
 			#if contents is HTML ???
 			print(url[1])
-			if (spiderleg.getTitle(url[1]) != None):
-				title = spiderleg.getTitle(url[1])
+			if (self.spiderleg.getTitle(url[1]) != None):
+				title = self.spiderleg.getTitle(url[1])
 			print("+++++++++++")
-			print(visited)
+			print(self.visited)
 			print("+++++++++++")
 			print("-----------")
-			print(unvisited)
+			print(self.unvisited)
 			print("-----------")
 			#images = spiderleg.getImages(url[1])
 			#meta = spiderleg.getMeta(url[1])
-			if (spiderleg.getHyperlink(url[1]) != None):
-				links = spiderleg.getHyperlink(url[1])
+			if (self.spiderleg.getHyperlink(url[1]) != None):
+				links = self.spiderleg.getHyperlink(url[1])
 				for link in links:
-					if (link not in visited) and (link not in unvisited) and (depth < maxDepth) and (link != None):
+					if (link not in self.visited) and (link not in self.unvisited) and (self.depth < self.maxDepth) and (link != None):
 						# need to actually check link is a link
 						parser = urlparse(link)
 						if (parser.scheme != '') and (parser.netloc != '') and (parser.scheme != None) and (parser.netloc != None):
-							unvisited.append([depth, link])
+							self.unvisited.append([self.depth, link])
 			print("====================")
+		
+		# the visited array - print this out once done crawling
 
-
-spider = Spider() #Spider.Spider() if importing this class
-spider.setup(unvisited, 5, "test")
-spider.crawl()
+	def getResults(self):
+		# return list of links that had keyword in meta
+		links = ["look", "im", "a", "pickle", "morty"]
+		return links
+	
+	
